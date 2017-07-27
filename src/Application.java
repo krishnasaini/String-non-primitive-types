9c1ad64bb15cf948e9d1fa5c9af4776b321c62b1
CHAITU SAINI <CHAITU SAINI@CHAITU>
2017-07-27 11:29:37 -0700

    String non primitive types

---
+++ src/Application.java
@@ -0,0 +1,25 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 7;
+		
+		String text = "Hello";
+
+		String blank = " ";
+
+		String name = "Krish";
+
+		String greeting = text + blank + name;
+
+		System.out.println(greeting);
+
+		System.out.println("Hello" + " " + "Krish");
+
+		System.out.println("My integer is:" + myInt);
+
+		double mydouble = 7.8;
+
+		System.out.println("my number is:" + mydouble + ".");
+	}
+
+}
